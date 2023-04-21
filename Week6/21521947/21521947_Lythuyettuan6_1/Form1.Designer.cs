d24661d6c53032671f8bbd78ca734791d66875e0
Henry2120 <100249282+Henry2120@users.noreply.github.com>
2023-04-21 23:06:42 +0700

    Add files via upload

---
+++ Week6/21521947/21521947_Lythuyettuan6_1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _21521947_Lythuyettuan6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.geturl = new System.Windows.Forms.Button();
+            this.displayurl = new System.Windows.Forms.ListBox();
+            this.pasteURL = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // geturl
+            // 
+            this.geturl.Location = new System.Drawing.Point(57, 48);
+            this.geturl.Name = "geturl";
+            this.geturl.Size = new System.Drawing.Size(178, 52);
+            this.geturl.TabIndex = 1;
+            this.geturl.Text = "button1";
+            this.geturl.UseVisualStyleBackColor = true;
+            this.geturl.Click += new System.EventHandler(this.geturl_Click);
+            // 
+            // displayurl
+            // 
+            this.displayurl.FormattingEnabled = true;
+            this.displayurl.ItemHeight = 16;
+            this.displayurl.Location = new System.Drawing.Point(12, 129);
+            this.displayurl.Name = "displayurl";
+            this.displayurl.Size = new System.Drawing.Size(776, 308);
+            this.displayurl.TabIndex = 3;
+            // 
+            // pasteURL
+            // 
+            this.pasteURL.Location = new System.Drawing.Point(336, 53);
+            this.pasteURL.Name = "pasteURL";
+            this.pasteURL.Size = new System.Drawing.Size(379, 22);
+            this.pasteURL.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pasteURL);
+            this.Controls.Add(this.displayurl);
+            this.Controls.Add(this.geturl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button geturl;
+        private System.Windows.Forms.ListBox displayurl;
+        private System.Windows.Forms.TextBox pasteURL;
+    }
+}
+
---
+++ Week6/21521947/21521947_Lythuyettuan6_1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+namespace _21521947_Lythuyettuan6_1
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void geturl_Click(object sender, EventArgs e)
+        {
+            geturl.Enabled = false;
+            var url = pasteURL.Text;
+            var todoList = await GetTodoList(url);
+            displayurl.Items.Clear();
+            int count = 0;
+            foreach (var task in todoList)
+            {
+                displayurl.Items.Add("userId: " + task.id);
+                displayurl.Items.Add("id: " + count);
+                displayurl.Items.Add("title: " + task.title);
+                displayurl.Items.Add("completed: " + task.completed);
+                displayurl.Items.Add("\n");
+                count++;
+            }
+            geturl.Enabled = true;
+        }
+
+        private async Task<dynamic[]> GetTodoList(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic[]>(content);
+        }
+    }
+}
