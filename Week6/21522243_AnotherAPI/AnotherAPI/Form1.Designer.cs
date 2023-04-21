f35ee7877235a4cedb79f7fdaca5ccd5ca83e037
Đỗ Phúc Kiên <128716623+dpkien2809@users.noreply.github.com>
2023-04-21 08:26:29 -0700

    Add files via upload

---
+++ Week6/21522243_AnotherAPI/AnotherAPI/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AnotherAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_GetAPI = new System.Windows.Forms.Button();
+            this.bt_Exit = new System.Windows.Forms.Button();
+            this.lv_Todos = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // bt_GetAPI
+            // 
+            this.bt_GetAPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_GetAPI.Location = new System.Drawing.Point(43, 44);
+            this.bt_GetAPI.Name = "bt_GetAPI";
+            this.bt_GetAPI.Size = new System.Drawing.Size(150, 60);
+            this.bt_GetAPI.TabIndex = 4;
+            this.bt_GetAPI.Text = "Get API";
+            this.bt_GetAPI.UseVisualStyleBackColor = true;
+            this.bt_GetAPI.Click += new System.EventHandler(this.bt_GetAPI_Click);
+            // 
+            // bt_Exit
+            // 
+            this.bt_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Exit.Location = new System.Drawing.Point(609, 44);
+            this.bt_Exit.Name = "bt_Exit";
+            this.bt_Exit.Size = new System.Drawing.Size(150, 60);
+            this.bt_Exit.TabIndex = 8;
+            this.bt_Exit.Text = "Exit";
+            this.bt_Exit.UseVisualStyleBackColor = true;
+            this.bt_Exit.Click += new System.EventHandler(this.bt_Exit_Click);
+            // 
+            // lv_Todos
+            // 
+            this.lv_Todos.HideSelection = false;
+            this.lv_Todos.Location = new System.Drawing.Point(12, 115);
+            this.lv_Todos.Name = "lv_Todos";
+            this.lv_Todos.Size = new System.Drawing.Size(776, 323);
+            this.lv_Todos.TabIndex = 9;
+            this.lv_Todos.UseCompatibleStateImageBehavior = false;
+            this.lv_Todos.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lv_Todos);
+            this.Controls.Add(this.bt_Exit);
+            this.Controls.Add(this.bt_GetAPI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_GetAPI;
+        private System.Windows.Forms.Button bt_Exit;
+        private System.Windows.Forms.ListView lv_Todos;
+    }
+}
+
---
+++ Week6/21522243_AnotherAPI/AnotherAPI/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+
+namespace AnotherAPI
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void bt_GetAPI_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    var todos = await GetTodosAsync();
+                    DisplayTodos(todos);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private async Task<List<Todo>> GetTodosAsync()
+        {
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/comments");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Todo>>(content);
+        }
+
+        private void DisplayTodos(List<Todo> todos)
+        {
+            lv_Todos.Items.Clear();
+            foreach (var todo in todos)
+            {
+                lv_Todos.Items.Add("postId: " + todo.postId + "\n");
+                lv_Todos.Items.Add("id: " + todo.id + "\n");
+                lv_Todos.Items.Add("name: " + todo.name + "\n");
+                lv_Todos.Items.Add("email: " + todo.email + "\n");
+                lv_Todos.Items.Add("body: " + todo.body + "\n");
+                lv_Todos.Items.Add("");
+            }
+        }
+        public class Todo
+        {
+            public int postId { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string body { get; set; }
+        }
+
+        private void bt_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
