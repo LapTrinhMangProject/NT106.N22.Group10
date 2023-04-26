a1b989a253379b051dcc0afdd11e4365a1bfe07a
NGODAT0103 <ngovuminhdat@gmail.com>
2023-04-26 17:02:40 +0700

    Thêm class player và statistic.

---
+++ Do An/Sourcecode/Server/Server/Response/Response_player_and_statistics.cs
@@ -0,0 +1,19 @@
+﻿using Library_football;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Response
+{
+    public class Response_player_and_statistics
+    {
+        Player Player { get; set; }
+        Statistics Statistics { get; set; }
+    }
+    public class Root_Response_player_and_statistics
+    {
+        public Response_player_and_statistics response { get; set; }
+    }
+}
---
+++ Do An/Sourcecode/Server/Server/league/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_football
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int age { get; set; }
+        public Birth birth { get; set; }
+        public string nationality { get; set; }
+        public string weight { get; set; }
+        public bool injured { get; set; }
+        public string photo { get; set; }
+    }
+    public class Birth
+    {
+        public string date { get; set; }
+        public string place { get; set; }
+        public string country { get; set; }
+    }
+}
