e6a55798b38da830a98a774275b2a0aa482c913d
LAPTOP-A1PPLVVM\thanh <vandat8080@gmail.com>
2023-04-29 23:06:39 +0700

    Cái này là version 1 của tui, cái version này phức tạp lắm, để tui làm cái khác đơn giản hơn. Nếu trễ deadline mà k thấy v2 của tui thì cứ nộp version này

---
+++ 21521947_ChatUDP_Version1/21521947_ChatUDP/UDP_server.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace _21521947_ChatUDP
+{
+    partial class UDP_server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstReceived = new System.Windows.Forms.ListBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtLocalPort = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstReceived
+            // 
+            this.lstReceived.FormattingEnabled = true;
+            this.lstReceived.ItemHeight = 16;
+            this.lstReceived.Location = new System.Drawing.Point(44, 107);
+            this.lstReceived.Name = "lstReceived";
+            this.lstReceived.Size = new System.Drawing.Size(518, 276);
+            this.lstReceived.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(586, 107);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(105, 50);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // txtLocalPort
+            // 
+            this.txtLocalPort.Location = new System.Drawing.Point(586, 220);
+            this.txtLocalPort.Name = "txtLocalPort";
+            this.txtLocalPort.Size = new System.Drawing.Size(105, 22);
+            this.txtLocalPort.TabIndex = 9;
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(9, 37);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(105, 22);
+            this.txtIpAddress.TabIndex = 10;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(9, 101);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(105, 22);
+            this.txtPort.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Port to transmit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 29);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "SERVER";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtIpAddress);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Location = new System.Drawing.Point(577, 274);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 138);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client Info";
+            // 
+            // UDP_server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocalPort);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lstReceived);
+            this.Name = "UDP_server";
+            this.Text = "UDP_server";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstReceived;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtLocalPort;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ 21521947_ChatUDP_Version1/21521947_ChatUDP/UDP_server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21521947_ChatUDP
+{
+    public partial class UDP_server : Form
+    {
+
+        string _localPort = "10";
+        string _remotePort = "1000";
+        UdpClient _applications = new UdpClient();
+        Thread _thread;
+        bool _exit = false;
+        delegate void ClearCacheReceivedData(string Data, string RemoteHost);
+        public UDP_server()
+        {
+            InitializeComponent();
+        }
+        private void ReceivedData(string Data, string RemoteHost)
+        {
+            if (lstReceived.InvokeRequired)
+            {
+                ClearCacheReceivedData clearCacheReceivedData = new ClearCacheReceivedData(ReceivedData);
+                lstReceived.Invoke(clearCacheReceivedData, new object[] { Data, RemoteHost });
+                return;
+            }
+            string msg = "";
+            msg = "(Sender: " + RemoteHost + ")" + Data;
+            lstReceived.Items.Insert(0, msg);
+        }
+        private void Explore()
+        {
+            IPAddress ip;
+            byte[] msg;
+            string str = "";
+            string name = Dns.GetHostName();
+            ip = Dns.GetHostEntry(name).AddressList[0];
+            IPEndPoint ep = new IPEndPoint(ip, Convert.ToInt16(_remotePort));
+            while (_exit == false)
+            {
+                Application.DoEvents();
+                if (_applications.Available > 0)
+                {
+                    msg = _applications.Receive(ref ep);
+                    str = System.Text.Encoding.UTF8.GetString(msg);
+                    ReceivedData(str, ep.Address.ToString());
+                }
+            }
+        }
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            _localPort = this.txtLocalPort.Text;
+            _remotePort = this.txtPort.Text;
+            _applications = new UdpClient(int.Parse(_localPort));
+            _thread = new Thread(Explore);
+            _thread.Start();
+            this.btnConnect.Enabled = false;
+            txtIpAddress.ReadOnly = txtLocalPort.ReadOnly = txtPort.ReadOnly = true;
+        }
+
+    }
+}
