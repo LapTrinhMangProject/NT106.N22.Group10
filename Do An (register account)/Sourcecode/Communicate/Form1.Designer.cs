f430eb20514f11ee9667f6642df9b793e317a4cc
LAPTOP-CV1CRQMN\acer <21522243@gm.uit.edu.vn>
2023-06-02 19:53:28 +0700

    cập nhật giao diện form đăng ký + đăng nhập

--- Do An (register account)/Sourcecode/Communicate/Form1.Designer.cs
+++ Do An (register account)/Sourcecode/Communicate/Form1.Designer.cs
@@ -30,13 +30,19 @@
         {
             this.server_button = new System.Windows.Forms.Button();
             this.client_button = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // server_button
             // 
-            this.server_button.Location = new System.Drawing.Point(224, 183);
+            this.server_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.server_button.Location = new System.Drawing.Point(437, 159);
+            this.server_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.server_button.Name = "server_button";
-            this.server_button.Size = new System.Drawing.Size(90, 38);
+            this.server_button.Size = new System.Drawing.Size(99, 42);
             this.server_button.TabIndex = 0;
             this.server_button.Text = "Server";
             this.server_button.UseVisualStyleBackColor = true;
@@ -44,24 +50,51 @@
             // 
             // client_button
             // 
-            this.client_button.Location = new System.Drawing.Point(394, 183);
+            this.client_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client_button.Location = new System.Drawing.Point(437, 238);
+            this.client_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.client_button.Name = "client_button";
-            this.client_button.Size = new System.Drawing.Size(90, 38);
+            this.client_button.Size = new System.Drawing.Size(99, 42);
             this.client_button.TabIndex = 1;
             this.client_button.Text = "Client";
             this.client_button.UseVisualStyleBackColor = true;
             this.client_button.Click += new System.EventHandler(this.client_button_Click);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Communicate.Properties.Resources.z4399127698614_8f048a4fd751b0e5a8c607b8d091248a;
+            this.pictureBox2.Location = new System.Drawing.Point(357, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(262, 91);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Communicate.Properties.Resources.giphy1;
+            this.pictureBox1.Location = new System.Drawing.Point(47, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(625, 418);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.client_button);
             this.Controls.Add(this.server_button);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +103,8 @@
 
         private System.Windows.Forms.Button server_button;
         private System.Windows.Forms.Button client_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Do An (register account)/Sourcecode/Communicate/Properties/Resources.Designer.cs
+++ Do An (register account)/Sourcecode/Communicate/Properties/Resources.Designer.cs
@@ -73,6 +73,76 @@ namespace Communicate.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap download {
+            get {
+                object obj = ResourceManager.GetObject("download", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download__1_ {
+            get {
+                object obj = ResourceManager.GetObject("download (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download__1_1 {
+            get {
+                object obj = ResourceManager.GetObject("download (1)1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GettyImages_1442587075_e1668806020544 {
+            get {
+                object obj = ResourceManager.GetObject("GettyImages-1442587075-e1668806020544", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap giphy {
+            get {
+                object obj = ResourceManager.GetObject("giphy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap giphy__1_ {
+            get {
+                object obj = ResourceManager.GetObject("giphy (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap giphy1 {
+            get {
+                object obj = ResourceManager.GetObject("giphy1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Icons8_Windows_8_Security_Password_2__1_ {
             get {
                 object obj = ResourceManager.GetObject("Icons8-Windows-8-Security-Password-2 (1)", resourceCulture);
@@ -83,9 +153,39 @@ namespace Communicate.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap images {
+            get {
+                object obj = ResourceManager.GetObject("images", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap images1 {
+            get {
+                object obj = ResourceManager.GetObject("images1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap user_icon_150670 {
             get {
                 object obj = ResourceManager.GetObject("user_icon_150670", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap z4399127698614_8f048a4fd751b0e5a8c607b8d091248a {
+            get {
+                object obj = ResourceManager.GetObject("z4399127698614_8f048a4fd751b0e5a8c607b8d091248a", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
