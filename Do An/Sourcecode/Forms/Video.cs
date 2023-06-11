f1a0f839c04a39b3c15857c67948ac60269af936
NGODAT0103 <ngovuminhdat@gmail.com>
2023-06-11 10:01:41 +0700

    Api jellyfin get Item and stream link

---
+++ Do An/Sourcecode/Forms/Video.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JellyFinAPI;
+
+namespace Forms
+{
+    public partial class Video : Form
+    {
+        public Video()
+        {
+            InitializeComponent();
+        }
+
+        private void Video_Load(object sender, EventArgs e)
+        {
+            Jellyfin jellyfin = new Jellyfin();
+            jellyfin.GetList();
+            foreach (var index in Jellyfin.requestStreamItems)
+            {
+                listBox1.Items.Add(index.Key);
+            }
+        }
+    }
+}
---
+++ Do An/Sourcecode/JellyFinAPI/Jellyfin.cs
@@ -0,0 +1,57 @@
+﻿using Emby.ApiClient;
+using Emby.ApiClient.Cryptography;
+using Emby.ApiClient.Model;
+using JellyFinAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace JellyFinAPI
+
+{
+    public class Jellyfin
+    {
+        public Jellyfin()
+        {
+        }
+        HttpClient client = new HttpClient();
+        static string api_key = JellyFinAPI.Properties.Resources.api_key;
+        static string ParentId = "f137a2dd21bbc1b99aa5c0f6bf02a805";
+        static public Dictionary<string, string> requestStreamItems = new Dictionary<string, string>();
+
+        ResponseJsonStructure responseJsonStructure = new ResponseJsonStructure();
+        public async void GetList()
+        {
+            WebRequest webRequest = WebRequest.Create($"http://localhost:8096/Items?Item=&api_key={api_key}&ParentId=f137a2dd21bbc1b99aa5c0f6bf02a805");
+            WebResponse response = webRequest.GetResponse();
+            Stream stream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(stream);
+            string data = reader.ReadToEnd();
+            responseJsonStructure = JsonConvert.DeserializeObject<ResponseJsonStructure>(data);
+            foreach (var index in responseJsonStructure.items)
+            {
+                MessageBox.Show(index.name);
+                string request = $"http://localhost:8096/Items/{index.id}/Download?api_key={api_key}";
+                requestStreamItems[index.name] = request;
+            }
+            FileStream file = new FileStream("test.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+            writer.Write(data);
+            writer.Flush();
+            writer.Close();
+            file.Close();
+            MessageBox.Show("Done");
+        }
+
+
+    }
+}
