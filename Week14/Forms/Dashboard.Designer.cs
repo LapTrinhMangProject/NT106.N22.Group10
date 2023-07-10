3b98e5f5c50060d0b16eab6c7de2c408e2853fdf
NGODAT0103 <ngovuminhdat@gmail.com>
2023-07-10 15:50:15 +0700

    Cập nhật tính năng duyệt phần bên trong thư mục

--- Week14/Forms/Dashboard.Designer.cs
+++ Week14/Forms/Dashboard.Designer.cs
@@ -28,32 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.directoryListbox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.uploadButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // directoryListbox
+            // button1
             // 
-            this.directoryListbox.FormattingEnabled = true;
-            this.directoryListbox.Location = new System.Drawing.Point(12, 23);
-            this.directoryListbox.Name = "directoryListbox";
-            this.directoryListbox.Size = new System.Drawing.Size(745, 199);
-            this.directoryListbox.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(12, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.type});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(343, 229);
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged_1);
+            this.listView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Dashboard_MouseDoubleClick);
+            // 
+            // name
+            // 
+            this.name.Text = "Tên Thư mục";
+            this.name.Width = 186;
+            // 
+            // type
+            // 
+            this.type.Text = "Kiểu Thư mục";
+            this.type.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.type.Width = 142;
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(193, 271);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 3;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            // 
+            // uploadButton
+            // 
+            this.uploadButton.Location = new System.Drawing.Point(307, 271);
+            this.uploadButton.Name = "uploadButton";
+            this.uploadButton.Size = new System.Drawing.Size(75, 23);
+            this.uploadButton.TabIndex = 4;
+            this.uploadButton.Text = "Upload";
+            this.uploadButton.UseVisualStyleBackColor = true;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.directoryListbox);
+            this.ClientSize = new System.Drawing.Size(471, 325);
+            this.Controls.Add(this.uploadButton);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.button1);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Dashboard_MouseDoubleClick);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox directoryListbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader type;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.Button uploadButton;
     }
 }
--- Week14/Forms/Dashboard.cs
+++ Week14/Forms/Dashboard.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using FluentFTP;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,13 +20,73 @@ namespace Forms
         {
             InitializeComponent();
         }
-
+        List<string> remoteSite = new List<string>();
+        string previousPath = "/";
         private void Dashboard_Load(object sender, EventArgs e)
         {
-            foreach (var nameDictionary in Login.client.GetNameListing())
+            remoteSite.Add(previousPath);
+            LoadFile();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            remoteSite.Remove(remoteSite.Last());
+            LoadFile();
+
+        }
+
+        private void ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        void LoadFile()
+        {
+            listView.Items.Clear();
+            int index = 0;
+            foreach (var item in Login.client.GetListing(remoteSite.Last()))
             {
-                directoryListbox.Items.Add(nameDictionary);
+                switch (item.Type)
+                {
+                    case FtpObjectType.Directory:
+                        ListViewItem tempItem = new ListViewItem(new[] { $"{item.Name}", $"{item.Type}" });
+                        listView.Items.Insert(index, tempItem);
+                        index++;
+                        break;
+
+                }
+
             }
+            foreach (var item in Login.client.GetListing(remoteSite.Last()))
+            {
+                switch (item.Type)
+                {
+                    case FtpObjectType.File:
+                        ListViewItem tempItem = new ListViewItem(new[] { $"{item.Name}", $"{item.Type}" });
+                        listView.Items.Insert(index, tempItem);
+                        index++;
+                        break;
+                }
+            }
+        }
+        private void Dashboard_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var tempData = listView.SelectedItems[0];
+            string newPath = tempData.SubItems[0].Text;
+            if (remoteSite.Count == 1)
+                remoteSite.Add(remoteSite[0] + newPath);
+            else
+            {
+                remoteSite.Add($"{previousPath}/{newPath}");
+                previousPath = remoteSite.Last();
+            }
+            LoadFile();
+        }
+
+        private void listView_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
